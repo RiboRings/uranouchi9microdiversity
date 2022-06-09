901669207d4d0537b6373ddafb80458da4009682
Giulio Benedetti <giuliobene2000@gmail.com>
2022-06-09 12:23:28 +0900

    Shift to ComplexHeatmap

--- snvs.R
+++ snvs.R
@@ -2,7 +2,9 @@ library(readr)
 library(dplyr)
 library(doParallel)
 library(reshape)
-library(ggplot2)
+library(tidyr)
+library(tibble)
+library(ComplexHeatmap)
 
 no_cores <- detectCores() - 1  
 cl <- makeCluster(no_cores, type = "FORK")  
@@ -12,8 +14,8 @@ files <- foreach(i = 1:9) %dopar% {
 
     read_csv2(paste0("data/snvs", i, ".csv")) %>%
     na.omit %>%
-#    filter(genome %in% c("metabat2bin_8.fna",
-#                         "metabat2bin_35.fna",
+    filter(genome %in% c("metabat2bin_8.fna",
+                         "metabat2bin_35.fna",
 #                         "metabat2bin_354.fna",
 #                         "metabat2bin_800.fna",
 #                         "metabat2bin_449.fna")) %>%
@@ -34,37 +36,46 @@ snvs_stats <- snvs_df %>%
 
 snvs_df <- snvs_df %>%
   left_join(snvs_stats) %>%
-  filter(Count >= 5,
-         mutation_type == "N")
+  filter(Count >= 3,
+         mutation_type %in% c("S", "N"))
 
 for (gen in unique(snvs_df$genome)) {
   
   genome_df <- snvs_df %>%
     filter(genome == gen) %>%
-    select(-scaffold, -allele_count) %>%
+    select(position,
+           con_freq,
+           sample,
+           mutation_type) %>%
+    group_by(position, sample) %>%
+    summarise(con_freq = mean(con_freq)) %>%
     mutate(sample = paste0("sample_", sample)) %>%
     pivot_wider(names_from = sample,
-                values_from = con_freq,
-                values_fill = 0) %>%
-    column_to_rownames(var = "position") %>%
-    select(starts_with("sample"))
+                values_from = con_freq) %>%
+    column_to_rownames(var = "position")
   
   names(genome_df) <- gsub("sample_", "", names(genome_df))
   genome_mat <-  genome_df[ , order(names(genome_df))] %>%
     as.matrix()
   
-  limits <- c(round(min(genome_mat), 1), round(max(genome_mat), 1))
+  limits <- c(round(min(genome_mat, na.rm = TRUE), 1), round(max(genome_mat, na.rm = TRUE), 1))
   breaks <- seq(limits[1], limits[2], by = 0.1)
   
-  Heatmap(genome_mat,
-          heatmap_legend_param = list(at = breaks),
-          col = c("darkblue", "blue", "white", "red", "darkred"),
-          name = "Base Consensus Frequency",
-          cluster_rows = TRUE,
-          cluster_columns = FALSE,
-          row_title = "Position",
-          column_title = "Sample",
-          row_names_gp = gpar(fontsize = 5))
+  pdf(paste0("results/", gen, "_snvs.pdf"),
+      width = 10,
+      height = 20)
+  
+  draw(Heatmap(genome_mat,
+               heatmap_legend_param = list(at = breaks),
+               col = c("white", "yellow", "orange", "red", "darkred"),
+               name = "Base Consensus Frequency",
+               cluster_columns = FALSE,
+               cluster_rows = FALSE,
+               row_title = "Position",
+               column_title = "Sample",
+               row_names_gp = gpar(fontsize = 5)))
+  
+  dev.off()
   
 #  genome_df <- snvs_df %>%
 #    filter(genome == gen)
@@ -87,11 +98,11 @@ for (gen in unique(snvs_df$genome)) {
 #    labs(x = "Sample",
 #         y = "Genomic Site",
 #         subtitle = gen)
-  
-    ggsave(paste0(gen, "_snvs.pdf"),
-           device = "pdf",
-           path = "results",
-           width = 10,
-           height = 20)
+#  
+#    ggsave(paste0(gen, "_snvs.pdf"),
+#           device = "pdf",
+#           path = "results",
+#           width = 10,
+#           height = 20)
 
 }
