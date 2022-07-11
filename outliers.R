ab7924f303ad42b4eeaf3cbe5993cde0da61f1d3
Giulio Benedetti <giuliobene2000@gmail.com>
2022-07-11 16:35:36 +0900

    Outlier workflow

--- outliers.R
+++ outliers.R
@@ -1,18 +1,3 @@
-library(dplyr)
-library(tidyr)
-library(readr)
-
-sample_loader <- function(sample_name, method = read_csv) {
-  
-  sample_df <- method(sample_name) %>%
-    mutate(sample = gsub(".csv", "", gsub("data/", "", sample_name))) %>%
-    left_join(mapping) %>%
-    filter(genome == "metabat2bin_835.fna") %>%
-    
-    return(sample_df)
-  
-}
-
 mapping <- read_tsv("data/mapping.stb", col_names = c("scaffold", "genome"))
 
 gene_info_files <- list.files(path = "data", pattern = "gene_info*")
@@ -29,5 +14,25 @@ gene_stats_df <- gene_info_df %>%
   summarise(pNpS = max(pNpS_variants)) %>%
   arrange(desc(pNpS))
 
-func_annot <- read_csv2("test.gff")
-write_csv2(left_join(gene_stats_df, func_annot), "data/new_func_annot.csv")
+func_annot <- read_csv2("data/func_annot.csv")
+
+func_annot_df <- func_annot %>%
+  group_by(product) %>%
+  summarise(gene,
+            pNpS,
+            Count = n()) %>%
+  arrange(desc(Count)) %>%
+  filter(!is.na(pNpS),
+         Count > 4)
+  
+p <- ggplot(func_annot_df, aes(y = pNpS, x = product)) +
+  geom_boxplot() +
+  labs(x = "Functional Group") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+ggsave("func_annot.pdf",
+       plot = p,
+       width = 15,
+       height = 7,
+       path = "results")
